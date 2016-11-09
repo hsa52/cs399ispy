479e03a4e69cabdb1b021fc43e0f921778577e22
Kaina <kaina.crow@gmail.com>
2016-11-08 20:48:03 -0700

    getting the video to load on the page when clicked. Added some functionality to navigate the pages

--- iSpyWithMyLittleEye/app/src/main/java/com/example/haydenpunchescamels/ispywithmylittleeye/devicelist.java
+++ iSpyWithMyLittleEye/app/src/main/java/com/example/haydenpunchescamels/ispywithmylittleeye/devicelist.java
@@ -1,7 +1,9 @@
 package com.example.haydenpunchescamels.ispywithmylittleeye;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class devicelist extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class devicelist extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.devicelist);
+
+        getSupportActionBar().hide(); // Hide action bar
+
+        initNavigationListeners();
     }
+
+    private void initNavigationListeners() {
+        // Set play button listener
+        findViewById(R.id.camera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), viewscreen.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 }
--- iSpyWithMyLittleEye/app/src/main/java/com/example/haydenpunchescamels/ispywithmylittleeye/viewscreen.java
+++ iSpyWithMyLittleEye/app/src/main/java/com/example/haydenpunchescamels/ispywithmylittleeye/viewscreen.java
@@ -3,6 +3,7 @@ package com.example.haydenpunchescamels.ispywithmylittleeye;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class viewscreen extends AppCompatActivity {
 
@@ -10,13 +11,17 @@ public class viewscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewscreen);
+
+        VideoView mVideoView = (VideoView) findViewById(R.id.videoView);
+        String uriPath = "android.resource://com.example.haydenpunchescamels.ispywithmylittleeye/" + R.raw.movie;
+        Uri uri2 = Uri.parse(uriPath);
+        mVideoView.setVideoURI(uri2);
+        mVideoView.requestFocus();
+        mVideoView.start();
     }
 
-    videoView mVideoView = (videoView) findViewById(R.id.videoView);
-    // VideoView mVideoView = new VideoView(this);
-    String uriPath = "android.resource://com.example.haydenpunchescamels.ispywithmylittleeye/" + R.raw.movie;
-    Uri uri2 = Uri.parse(uriPath);
-    mVideoView.setVideoURI(uri2);
-    mVideoView.requestFocus();
-    mVideoView.start();
+
+
+
+
 }
